51acaa1d37ef9143db53382868a7d12af8a1ce4e
lucasSCsantos <luc.cristovam10@gmail.com>
2021-03-24 15:31:38 -0300

    parte1_aula

---
+++ bloco_3/dia_5/Parte 1/scripts.js
@@ -0,0 +1,10 @@
+const fullName = "Lucas Santana Cristovam dos Santos";
+const birthCity = "Salvador";
+let birthYear = 2001;
+
+birthYear = 2020;
+// birthCity = "Vitoria da Conquista";
+
+console.log(fullName);
+console.log(birthCity);
+console.log(birthYear);
